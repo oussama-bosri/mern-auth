9e3e54078702c50ba8985372b2e5522d6717ed6a
OUSSAMA <58121982+oussama-bosri@users.noreply.github.com>
2023-03-19 11:27:32 +0100

    first commit

---
+++ utils/connectdb.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const url = process.env.MONGO_DB_CONNECTION_STRING
+const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  //useCreateIndex: true,
+  //useFindAndModify: false ,
+})
+connect
+  .then(db => {
+    console.log("connected to db")
+  })
+  .catch(err => {
+    console.log(err)
+  })
